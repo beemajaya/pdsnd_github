c8f5f16186467316d09c50483e46adb77e60a048
beemajaya <beemajaya@hotmail.com>
2020-09-11 20:48:25 +0100

    Add matplotlib package

--- bikeshare.py
+++ bikeshare.py
@@ -2,6 +2,7 @@ import time
 import datetime as dt
 import pandas as pd
 import numpy as np
+import matplotlib as plt
 
 CITY_DATA = { 'chicago': 'chicago.csv',
               'new york city': 'new_york_city.csv',
